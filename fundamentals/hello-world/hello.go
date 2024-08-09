2669e79c88cce8b6caf8ef25ac27f233b37624e8
Gabriel Almeida <gabriel.dev.almeid@outlook.com>
2024-08-09 01:33:04 -0300

    hello world - add spanish

--- fundamentals/hello-world/hello.go
+++ fundamentals/hello-world/hello.go
@@ -2,16 +2,20 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
+const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return helloPrefix + name
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Gabriel"))
+	fmt.Println(Hello("Gabriel", "Spanish"))
 }
